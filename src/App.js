e84c6be7d08158ef346f34de63c3d350042a00e0
Jonel Getigan <jonel.getigan@gmail.com>
2020-09-21 07:13:06 +0800

    set up login

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
-import React, { Component } from "react"
+import React, {useState} from 'react';
+import './App.css';
+import Header from './components/Header/Header';
+import LoginForm from './components/LoginForm/LoginForm';
+import RegistrationForm from './components/RegistrationForm/RegistrationForm';
+import Home from './components/Home/Home';
+import PrivateRoute from './utils/PrivateRoute';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import AlertComponent from './components/AlertComponent/AlertComponent';  
 
-// import logo from "logo.svg";
-import AppointmentApp from "./components/AppointmentApp.js";
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
-import "./App.css";
-
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <MuiThemeProvider>
-        <AppointmentApp/>
-        </MuiThemeProvider>
-      </div>
-    );
-  }
+function App() {
+  const [title, updateTitle] = useState(null);
+  const [errorMessage, updateErrorMessage] = useState(null);
+  
+  return (
+    <Router>
+    <div className="App">
+      <Header title={title}/>
+        <div className="container d-flex align-items-center flex-column">
+          <Switch>
+            <Route path="/" exact={true}>
+              <RegistrationForm showError={updateErrorMessage} updateTitle={updateTitle}/>
+            </Route>
+            <Route path="/register">
+              <RegistrationForm showError={updateErrorMessage} updateTitle={updateTitle}/>
+            </Route>
+            <Route path="/login">
+              <LoginForm showError={updateErrorMessage} updateTitle={updateTitle}/>
+            </Route>
+            <PrivateRoute path="/home">
+              <Home/>
+            </PrivateRoute>
+          </Switch>
+          <AlertComponent errorMessage={errorMessage} hideError={updateErrorMessage}/>
+        </div>
+    </div>
+    </Router>
+  );
 }
+
 export default App;
